6ddf46f512c1411d7bab59f51aeebee3b9b0d243
Tomas Krušinskas <tomas.krusinskas@ktu.edu>
2024-06-25 23:05:34 +0300

    Initial

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string | null = null;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.errorMessage = '0 Rizz, no access';
+    }
+  }
+}
---
+++ src/app/components/services/services.component.ts
@@ -0,0 +1,69 @@
+import {Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-services',
+  templateUrl: './services.component.html',
+  styleUrl: './services.component.scss'
+})
+export class ServicesComponent implements OnInit, OnDestroy {
+  footerText: string = '';
+  private texts: string[] = ['one step beyond', '@ cyberia 2024'];
+  private currentTextIndex: number = 0;
+  private intervalId: any;
+
+  @ViewChild('footerTextElement') footerTextElement!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.animateText();
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  private animateText() {
+    const currentText = this.texts[this.currentTextIndex];
+    this.typeText(currentText).then(() => {
+      setTimeout(() => {
+        this.deleteText(currentText).then(() => {
+          this.currentTextIndex = (this.currentTextIndex + 1) % this.texts.length;
+          this.animateText();
+        });
+      }, 2000);
+    });
+  }
+
+  private typeText(text: string): Promise<void> {
+    return new Promise((resolve) => {
+      let index = 0;
+      this.intervalId = setInterval(() => {
+        if (index < text.length) {
+          this.footerText += text.charAt(index);
+          index++;
+        } else {
+          clearInterval(this.intervalId);
+          resolve();
+        }
+      }, 100);
+    });
+  }
+
+  private deleteText(text: string): Promise<void> {
+    return new Promise((resolve) => {
+      let index = text.length;
+      this.intervalId = setInterval(() => {
+        if (index > 0) {
+          this.footerText = this.footerText.substring(0, index - 1);
+          index--;
+        } else {
+          clearInterval(this.intervalId);
+          resolve();
+        }
+      }, 100);
+    });
+  }
+}
